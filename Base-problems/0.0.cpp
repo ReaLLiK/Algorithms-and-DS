0fbafe07b4a16d32c7b1c7a3c481e173186c18d1
ReaLLiK <148726976+ReaLLiK@users.noreply.github.com>
2023-10-31 11:42:51 +0300

    Update 0.0.cpp

--- Base-problems/0.0.cpp
+++ Base-problems/0.0.cpp
@@ -1,1 +1,30 @@
 
+#include <iostream>
+#include <fstream>
+#include <set>
+#include <numeric>
+using namespace std;
+
+int main()
+{
+    ifstream in;
+    ofstream out;
+    long long sum=0;
+    in.open("input.txt");
+    out.open("output.txt");
+    long a;
+    set <int> tree;
+    while (!in.eof())
+    {
+        in >> a;
+        tree.insert(a);
+    }
+    for (set<int>::iterator iterator = tree.begin(); iterator != tree.end(); ++iterator) 
+    {
+        sum += *iterator;
+    }
+    out<<sum;
+    in.close();
+    out.close();
+    return 0;
+}
